5ec2161b2364100dec0e15e6af2389f5aa7175e5
Serhii <serhii.domik@gmail.com>
2025-06-13 14:25:57 +0300

    fixed Testing settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -57,3 +57,6 @@ class TestingSettings(Settings):
     JWT_SIGNING_ALGORITHM: str = "HS256"
     PATH_TO_DB: str = ":memory:"
     MAILHOG_API_PORT: int = 8025
+
+    class Config:
+        extra = "allow"
